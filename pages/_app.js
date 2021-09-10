6dca7517ece6b8dc3c31a6d33cd833da6c64e6c6
ritvik-plabs <ritvikbhatia99@gmail.com>
2021-09-11 02:27:04 +0530

    background

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,29 @@ import config from "../config/particlesjs-config";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Particles
-        style={{ position: "absolute", width: "100%", height: "100%" }}
-        params={config}
-      ></Particles>
+      <div
+        style={{
+          position: "fixed !important",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100% !important",
+          backgroundColor: "gray",
+          zIndex: -2,
+        }}
+      >
+        <Particles
+          style={{
+            position: "fixed !important",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100% !important",
+            zIndex: 0,
+          }}
+          params={config}
+        ></Particles>
+      </div>
       <Component {...pageProps}></Component>
     </>
   );
