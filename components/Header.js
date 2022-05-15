8f6987a077ac38aa6dfb9580be24dfa8fa7c56a7
Shahbaz Ahmad <shahbazahmad@Shahbazs-MacBook-Air.local>
2022-05-15 22:56:39 +0530

    add main page photoshop

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,33 @@
 import { Box, Button } from "../styles";
 import { FileText, Briefcase, Layers } from "@geist-ui/react-icons";
 import { colors } from "../styles/theme";
+import Link from "next/Link";
 
 export default function Tags() {
   return (
     <Box type="row" px={4} py={2}>
-      <Button>Ritvik Bhatia</Button>
+      <Link href="/">
+        <Button>Ritvik Bhatia</Button>
+      </Link>
       <Box type="row">
-        <Box type="row">
-          <Briefcase color={colors?.primary}></Briefcase>
-          <Button>Experience</Button>
-        </Box>
-        <Box type="row" px={3}>
-          <Layers color={colors?.primary}></Layers>
-          <Button>Projects</Button>
-        </Box>
-        <Box type="row">
-          <FileText color={colors?.primary}></FileText>
-          <Button>Resume</Button>
-        </Box>
+        <Link href="/experience">
+          <Box type="row">
+            <Briefcase color={colors?.primary}></Briefcase>
+            <Button>Experience</Button>
+          </Box>
+        </Link>
+        <Link href="/projects">
+          <Box type="row" px={3}>
+            <Layers color={colors?.primary}></Layers>
+            <Button>Projects</Button>
+          </Box>
+        </Link>
+        <Link href="/resume">
+          <Box type="row">
+            <FileText color={colors?.primary}></FileText>
+            <Button>Resume</Button>
+          </Box>
+        </Link>
       </Box>
     </Box>
   );
--- pages/projects.js
+++ pages/projects.js
@@ -8,11 +8,7 @@ export default function Home() {
   return (
     <Box type="row" p={4}>
       <Box>
-        <H5 mb={0}>Hey, my name is</H5>
-        <H1 mt={0}>Ritvik Bhatia</H1>
-      </Box>
-      <Box>
-        <Image height={700} width={850} src={"/images/main.png"}></Image>
+        <H5 mb={0}>Projects</H5>
       </Box>
     </Box>
   );
